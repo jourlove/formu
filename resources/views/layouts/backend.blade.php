edcdcbf3e5e089560fe7f166df79b245e0a3dc89
jiangsongyi0204 <jiangsongyi0204@gmail.com>
2017-11-17 14:09:28 +0900

    show admin user when admin login

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -45,13 +45,13 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        @if (Auth::guest())
+                        @if (Auth::guard('admin')->guest())
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::guard('admin')->user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
