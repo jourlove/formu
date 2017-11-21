c27ae5fcd0f3458f371eaf8f76f8caf14abff364
jiangsongyi0204 <jiangsongyi0204@gmail.com>
2017-11-21 12:30:30 +0900

    template modify

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,9 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            @include('admin.sidebar')
-
-            <div class="col-md-9">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">Users</div>
                     <div class="panel-body">
